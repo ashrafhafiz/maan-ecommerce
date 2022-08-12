64f40e55f6a2e281161d688834eb32e8e74d7e4b
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2022-08-12 19:59:55 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('components.dashboard');
+});
+
+
+Route::get('/store', function () {
+    return view('components.store');
+});
